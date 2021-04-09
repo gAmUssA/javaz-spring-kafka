4bb21f07b6779ef06296a54b2b7c286cde2c59d0
Viktor Gamov <viktor@confluent.io>
2021-04-09 00:42:30 -0400

    initial commit

---
+++ src/test/java/io/confluent/developer/javaz/JavazSpringKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.confluent.developer.javaz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavazSpringKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
